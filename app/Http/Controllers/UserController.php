9bbd5ddf3d8f9ac7e7631e19e92853f1a5e214d2
emmkall <ing.emmanuel.cal@gmail.com>
2023-04-29 14:20:15 -0600

    punto 4b y 4c, get edad of every user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,6 +13,13 @@ class UserController extends Controller
         $users = DB::table( 'users' )
             ->join( 'user_domicilios', 'users.id', '=', 'user_domicilios.user_id' )
             ->get();
+            foreach ($users as $key => $value) {
+                $now = Carbon::now();
+                $fecha_nacimiento = Carbon::createFromFormat( 'Y-m-d', $value->fecha_nacimiento );
+                $year_nacimiento = $fecha_nacimiento->year;
+                $edad = $now->subYears( $year_nacimiento );
+                $value->edad = $edad->year;
+            }
         return response()->json( $users );
     }
 }
