e3721f510eb0ac415ac7b06b02810c554a478571
emmkall <ing.emmanuel.cal@gmail.com>
2023-04-29 14:10:49 -0600

    Test tables

--- database/migrations/2023_04_29_193542_create_user_domicilio_table.php
+++ database/migrations/2023_04_29_193542_create_user_domicilio_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_domicilio', function (Blueprint $table) {
+        Schema::create('user_domicilios', function (Blueprint $table) {
             $table->id();
             $table->foreignId( 'user_id' )->constrained()->onDelete( 'cascade' );
             $table->string( 'domicilio' );
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_domicilio');
+        Schema::dropIfExists('user_domicilios');
     }
 };
