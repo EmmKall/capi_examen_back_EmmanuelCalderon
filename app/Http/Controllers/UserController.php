4fe9b419ed2590f58791cb037b07fafb773dba13
emmkall <ing.emmanuel.cal@gmail.com>
2023-04-29 14:17:16 -0600

    Punto 4, created controller to show all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = DB::table( 'users' )
+            ->join( 'user_domicilios', 'users.id', '=', 'user_domicilios.user_id' )
+            ->get();
+        return response()->json( $users );
+    }
 }
--- app/Models/UserDomicilio.php
+++ app/Models/UserDomicilio.php
@@ -9,6 +9,8 @@ class UserDomicilio extends Model
 {
     use HasFactory;
 
+    protected $fillable = [ 'user_id', 'domicilio', 'numero_exterior', 'colonia', 'cp', 'ciudad' ];
+
     public function user()
     {
         return $this->belongsTo( User::class );
